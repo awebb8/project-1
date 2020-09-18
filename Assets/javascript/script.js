006ef3df883301f8b353d7d53f07b5b927c7450b
David Pan <dpan5425@gmail.com>
2020-09-18 10:59:42 -0400

    added showRestaurantsResults function

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,28 +4,24 @@ $(document).ready(function() {
     const ZOMATOAPIKEY = "9723ed5e62a95b63e3f30544b70f8fdb";
     const SPOONAPIKEY = "c27de7a75c74494f8fe916321c2ede25";
 
-
-    // $.ajax({
-    //     url: "https://api.spoonacular.com/recipes/654959/information?apiKey=c27de7a75c74494f8fe916321c2ede25",
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
-
     $("#search-button").on("click", function() {
         // Store user search into searchQuery variable
         var searchQuery = $("#search-input").val();
+        // Clear localStorage to prevent past searches from breaking logic
+        localStorage.clear();
 
         // Check which radio button user selected
         if($("input[type=radio]:checked", ".radiobuttons").val() === "option1") {
             // User selected Eat In - Make AJAX call to Spoonacular
             $.ajax({
-                url: `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&apiKey=${SPOONAPIKEY}`,
+                url: `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&apiKey=${SPOONAPIKEY}&addRecipeInformation=true`,
                 method: "GET"
             }).then(function(response) {
                 console.log(response);
                 // Store the response in localStorage so we can retrieve it later in a seperate JS file
                 localStorage.setItem("recipesSearchResults", JSON.stringify(response));
+                // Store a value in localStorage to determine which radio button user selected
+                localStorage.setItem("userRadioButtonOption", "Eat in");
 
                 // After making AJAX call, redirect user to search-results.html
                 window.location.href = "search-results.html";                    
--- results-script.js
+++ results-script.js
@@ -8,8 +8,15 @@ $(document).ready(function() {
     var recipesSearchResults = JSON.parse(localStorage.getItem("recipesSearchResults"));
     var restaurantsSearchResult = JSON.parse(localStorage.getItem("restaurantsSearchResults"));
 
-    console.log(recipesSearchResults);
-    console.log(restaurantsSearchResult);
+    // Check which radio button user selected - value is retrieved from localStorage
+    if(localStorage.getItem("userRadioButtonOption")) {
+        console.log(recipesSearchResults);
+        showRecipeResults();
+    }
+    else {
+        console.log(restaurantsSearchResult);
+        showRestaurantResults();
+    }
     
     // add array of objects to simulate taking results from local storage api data.
     // array of objects is goal for the rest of today.
@@ -18,57 +25,140 @@ $(document).ready(function() {
     //recipe version depending on the choice made.
     //stretch goal for today. goal for friday.
     
-    function showResults() {
-    
-    
+    // Dynamically update search-results.html with response from Spoonacular
+    function showRecipeResults() {
     $("primary-row").empty();
     for(var i=0; i<10; i++){
         var resultsCard = $("<div class='card card-body col-sm-3 results-card'>");
     
+        var resultsA = $("<a href='#'>");
+
         var resultsImg = $("<img class='card-img-top' alt='placeholder'>");
-        resultsImg.attr("src", "http://placekitten.com/300/300");
+        resultsImg.attr("src", recipesSearchResults.results[i].image);
+        resultsImg.attr("data-id", recipesSearchResults.results[i].id);
     
         var resultsTitle = $("<h6 class='card-header'>");
-        resultsTitle.text("Restaurant/recipe name");
+        resultsTitle.text(recipesSearchResults.results[i].title);
     
-        var resultsList =$("<ul class='list-group list-group-flush'>")
+        // var resultsList =$("<ul class='list-group list-group-flush'>")
     
-        var ratingLine = $("<li class='list-group-item'>");
-        ratingLine.text("Rating");
+        // var ratingLine = $("<li class='list-group-item'>");
+        // ratingLine.text("Rating");
     
-        var locationLine = $("<li class='list-group-item'>");
-        locationLine.text("location");
+        // var locationLine = $("<li class='list-group-item'>");
+        // locationLine.text("location");
     
-        var cuisineLine = $("<li class='list-group-item'>");
-        cuisineLine.text("Cuisine Type");
+        // var cuisineLine = $("<li class='list-group-item'>");
+        // cuisineLine.text("Cuisine Type");
     
-        var costLine = $("<li class='list-group-item'>");
-        costLine.text("Cost");
+        // var costLine = $("<li class='list-group-item'>");
+        // costLine.text("Cost");
     
-        var hoursLine = $("<li class='list-group-item'>");
-        hoursLine.text("Hours");
+        // var hoursLine = $("<li class='list-group-item'>");
+        // hoursLine.text("Hours");
     
-        resultsList.append(
-            ratingLine,
-            locationLine,
-            cuisineLine,
-            costLine,
-            hoursLine
-        );
+        // resultsList.append(
+        //     ratingLine,
+        //     locationLine,
+        //     cuisineLine,
+        //     costLine,
+        //     hoursLine
+        // );
     
+        resultsA.append(resultsImg);
+
         resultsCard.append(
-            resultsImg,
+            resultsA,
             resultsTitle,
-            resultsList   
+            // resultsList   
         );
     
         $("#primary-row").append(resultsCard);
             
         }
     }
-    
-    showResults();
-    
+
+    // Dynamically update search-results.html with response from Zomato
+    function showRestaurantResults() {
+        $("primary-row").empty();
+        for(var i=0; i<10; i++){
+
+
+            // AJAX call to Yelp to grab images
+            $.ajax({
+                url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${restaurantsSearchResult.restaurants[i].restaurant.name}&location=${restaurantsSearchResult.restaurants[i].restaurant.location.city}`,
+                headers: {
+                    'Authorization':'Bearer xQhqk6iKwI8rprXKZO006ZmPHJq4VXj3uHMkRTQl3Otj3mbw86-ZTGwW6CH5BEjuAY8pmnrwFGV3ReAAXIyh9c4o8SxD2yev4EXkaS9Ny9_MXdQq01_urRTbo45jX3Yx',
+                },
+                method: "GET",
+                dataType: 'json'
+            }).then(function(response) {
+                console.log(response);
+            });
+
+
+
+
+
+
+
+            var resultsCard = $("<div class='card card-body col-sm-3 results-card'>");
+        
+            // var resultsImg = $("<img class='card-img-top' alt='placeholder'>");
+            // resultsImg.attr("src", recipesSearchResults.results[i].image);
+        
+            var resultsTitle = $("<h6 class='card-header'>");
+            resultsTitle.text(restaurantsSearchResult.restaurants[i].restaurant.name);
+        
+            // var resultsList =$("<ul class='list-group list-group-flush'>")
+        
+            // var ratingLine = $("<li class='list-group-item'>");
+            // ratingLine.text("Rating");
+        
+            // var locationLine = $("<li class='list-group-item'>");
+            // locationLine.text("location");
+        
+            // var cuisineLine = $("<li class='list-group-item'>");
+            // cuisineLine.text("Cuisine Type");
+        
+            // var costLine = $("<li class='list-group-item'>");
+            // costLine.text("Cost");
+        
+            // var hoursLine = $("<li class='list-group-item'>");
+            // hoursLine.text("Hours");
+        
+            // resultsList.append(
+            //     ratingLine,
+            //     locationLine,
+            //     cuisineLine,
+            //     costLine,
+            //     hoursLine
+            // );
+        
+            resultsCard.append(
+                // resultsImg,
+                resultsTitle,
+                // resultsList   
+            );
+        
+            $("#primary-row").append(resultsCard);
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
